7f2a0ba0360771a2de31b6447d21645ebc9c1139
Mehran Fallah <m.mehran.fallah@gmail.com>
2023-07-22 00:46:45 +0430

    Code clean up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,20 +10,13 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        
-        // This part available on course, but I don't use it because it's useless
-        /*if (transform.position.y<-9)
-        {
-            transform.position = new Vector3(Random.Range(-9, 9),
-                Random.Range(8, 10), 0);
-        } */
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Player player = other.GetComponent<Player>();
+            var player = other.GetComponent<Player>();
             if(player!=null) player.Damage();
             Destroy(gameObject);
         }
