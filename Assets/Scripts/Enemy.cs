90328dcd5a51c142dc1d8d39d805873a4ba2d27b
Mehran Fallah <m.mehran.fallah@gmail.com>
2023-07-22 00:40:04 +0430

    Create OnTriggerEnter event
    check who collided to enemy and deciding what to do next

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float _speed = 5;
+    private float _speed = 4;
 
     void Update()
     {
@@ -16,5 +17,20 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(Random.Range(-9, 9),
                 Random.Range(8, 10), 0);
         } */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            if(player!=null) player.Damage();
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
